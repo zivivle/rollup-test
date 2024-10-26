2038000d6d7eae602eb077fb1f05fbd1dfc69480
zivivle <123868471+zivivle@users.noreply.github.com>
2024-10-26 19:20:16 +0900

    fix: 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import styles from "./page.module.css";
 import { Button } from "zivivle-rollup";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h2>rollup test!</h2>
-      <Button
-        label="rollup component!!!"
-        onClick={() => {}}
-        type="button"
-        className=""
-      />
+      <Button label="rollup component!!!" onClick={() => {}} type="button" />
     </div>
   );
 }
