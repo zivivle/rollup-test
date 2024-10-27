06cb4c2a941bc612dfd5ba49d44de62aceaf2323
zivivle <123868471+zivivle@users.noreply.github.com>
2024-10-27 22:31:41 +0900

    fix: 버그 수정중

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 // import localFont from "next/font/local";
-import "zivivle-rollup/dist/styles.css";
+// import "zivivle-rollup/dist/styles.css";
 import "./globals.css";
 
 // const geistSans = localFont({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { Button } from "zivivle-rollup";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.page}>
       <h2>rollup test!</h2>
-      <Button variant="solid" type="secondary" onClick={() => alert("Hello")}>
+      <Button variant="solid" type="primary" onClick={() => alert("Hello")}>
         클릭!!!
       </Button>
     </div>
