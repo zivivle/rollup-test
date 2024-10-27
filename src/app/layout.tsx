da5cfab3c475aff4feb5014783ca2937a4331ee0
zivivle <123868471+zivivle@users.noreply.github.com>
2024-10-27 21:17:46 +0900

    bug: 버그수정중

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
+import "zivivle-rollup/dist/styles.css";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body
+      // className={`${geistSans.variable} ${geistMono.variable}`}
+      >
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
 "use client";
 
 import { Button } from "zivivle-rollup";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div>
       <h2>rollup test!</h2>
-      <Button
-        variant="solid"
-        type="secondary"
-        size="large"
-        disabled={false}
-        onClick={() => {}}
-      >
-        버튼
+      <Button variant="solid" type="secondary" onClick={() => alert("Hello")}>
+        클릭!!!
       </Button>
     </div>
   );
